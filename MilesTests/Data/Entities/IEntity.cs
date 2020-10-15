9147a203d964234c387236f8144b9bd2f3d39657
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-10-15 15:37:51 +0100

    Status added to IEntity

--- MilesTests/Data/Entities/IEntity.cs
+++ MilesTests/Data/Entities/IEntity.cs
@@ -20,5 +20,15 @@ namespace MilesBackOffice.Web.Data.Entities
 
 
         bool IsConfirm { get; set; }
+
+        /// <summary>
+        /// Defines the object current status:
+        /// 0 - Approved by a SU
+        /// 1 - Waiting approval by a SU
+        /// 2 - Returned by a SU for editing
+        /// 3 - To be deleted
+        /// 4 - Someother state
+        /// </summary>
+        int Status { get; set; }
     }
 }
