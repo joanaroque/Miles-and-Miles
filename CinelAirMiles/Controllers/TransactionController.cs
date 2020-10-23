2caa6c06a17499d3bf1a3f7faf1f2bbbdcbd0079
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-10-23 17:35:50 +0100

    ClientViews(incomplete)

---
+++ CinelAirMiles/Controllers/TransactionController.cs
@@ -0,0 +1,66 @@
+﻿namespace CinelAirMiles.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using CinelAirMiles.Models;
+
+    public class TransactionController : Controller
+    {
+
+        public TransactionController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult MilesIndex()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult TransactionIndex()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Purchase()
+        {
+            return PartialView("_Purchase");
+        }
+
+        [HttpGet]
+        public IActionResult Extend()
+        {
+            return PartialView("_Extend");
+        }
+
+        [HttpGet]
+        public IActionResult Conversion()
+        {
+            return PartialView("_Conversion");
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult AddTransaction(TransactionViewModel model)
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult NominateToGold()
+        {
+            return PartialView();
+        }
+
+
+    }
+}
---
+++ CinelAirMiles/Models/TransactionViewModel.cs
@@ -0,0 +1,47 @@
+﻿namespace CinelAirMiles.Models
+{
+    using CinelAirMilesLibrary.Common.Data.Entities;
+    using CinelAirMilesLibrary.Common.Enums;
+
+    public class TransactionViewModel
+    {
+        public int Id { get; set; }
+
+
+        public int StartBalance { get; set; }
+
+        public int EndBalance { get; set; }
+
+        /// <summary>
+        /// In miles
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// 0 - Debit,
+        /// 1 - Credit,
+        /// 2 - Loan,
+        /// 3 - Purchase,
+        /// 4 - Transfer,
+        /// 5 - Extension
+        /// </summary>
+        public TransactionType Type { get; set; }
+
+        public User TransferTo { get; set; }
+
+        /// <summary>
+        /// Price of the transaction
+        /// Applicable in:
+        /// - Purchase of miles
+        /// - Transfer
+        /// - Extend
+        /// - Conversion
+        /// 
+        /// In Euros
+        /// </summary>
+        public decimal Price { get; set; }
+
+
+        public PremiumOffer Product { get; set; }
+    }
+}
