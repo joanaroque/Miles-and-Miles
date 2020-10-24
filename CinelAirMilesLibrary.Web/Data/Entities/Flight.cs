cedeb52301e6453f30a318a960f928e83e2f0b1e
joanaroque <joanatpsi@gmail.com>
2020-10-24 16:15:07 +0100

    added flight entity

---
+++ CinelAirMilesLibrary.Web/Data/Entities/Flight.cs
@@ -0,0 +1,56 @@
+﻿namespace CinelAirMilesLibrary.Common.Data.Entities
+{
+    using System;
+
+
+    public class Flight : IEntity
+    {
+        public string Departure { get; set; }
+
+
+
+        public string Arrival { get; set; }
+
+
+
+        public int MaximumSeats { get; set; }
+
+
+
+        public int AvailableSeats { get; set; }
+
+
+
+        public Partner Partner { get; set; }
+
+
+
+
+
+        public int Miles { get; set; }
+
+
+
+        public int Id { get; set; }
+
+
+
+        public User CreatedBy { get; set; }
+
+
+
+        public DateTime CreateDate { get; set; }
+
+
+
+        public DateTime UpdateDate { get; set; }
+
+
+
+        public User ModifiedBy { get; set; }
+
+
+
+        public int Status { get; set; }
+    }
+}
