62fd5205d1bffd66232fbeb54460b0264bfe47d1
Catia Oliveira <ctoliveira44@gmail.com>
2020-10-25 11:15:41 +0000

    Added accountController, MailHelper and user viewmodels: change user, recover password, change password, login, register

--- CinelAirMiles/Controllers/ComplaintController.cs
+++ CinelAirMiles/Controllers/ComplaintController.cs
@@ -3,20 +3,20 @@
     using global::CinelAirMiles.Data.Repositories;
     using global::CinelAirMiles.Helpers;
     using global::CinelAirMiles.Models;
-
     using Microsoft.AspNetCore.Mvc;
-
+    using MilesBackOffice.Web.Helpers;
+    using System;
     using System.Threading.Tasks;
 
     public class ComplaintController : Controller
     {
         private readonly IComplaintRepository _complaintRepository;
-        private readonly IUserHelperClient _userHelper;
+        private readonly IUserHelper _userHelper;
         private readonly IClientConverterHelper _converterHelper;
 
         public ComplaintController(
             IComplaintRepository complaintRepository,
-            IUserHelperClient userHelper,
+            IUserHelper userHelper,
             IClientConverterHelper converterHelper)
         {
             _complaintRepository = complaintRepository;
@@ -43,33 +43,33 @@
         [HttpPost]
         public async Task<IActionResult> Create(ComplaintViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                //try
-                //{
-                //    var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
-                //    if (user == null)
-                //    {
-                //        //todo criar erro
-                //    }
+            //if (ModelState.IsValid)
+            //{
+            //    try
+            //    {
+            //        var user = await _userHelper.GetUserByEmailAsync(User.Identity.Name);
+            //        if (user == null)
+            //        {
+            //            //todo criar erro
+            //        }
 
-                //    var complaint = _converterHelper.ToComplaintClientViewModel(model);
+            //        var complaint = _converterHelper.ToComplaintClientViewModel(model);
 
-                //    var result = await _complaintRepository.CreateAsync(complaint);
+            //        var result = await _complaintRepository.CreateAsync(complaint);
 
-                //    if (!result)
-                //    {
-                //        //todo criar erro
-                //    }
+            //        if (!result)
+            //        {
+            //            //todo criar erro
+            //        }
 
-                //}
-                //catch (Exception ex)
-                //{
-                //    ModelState.AddModelError(string.Empty, ex.Message);
-                //}
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        ModelState.AddModelError(string.Empty, ex.Message);
+            //    }
 
-                //return RedirectToAction(nameof(Index));
-            }
+            //    return RedirectToAction(nameof(Index));
+            //}
             return RedirectToAction(nameof(Index));
         }
     }
