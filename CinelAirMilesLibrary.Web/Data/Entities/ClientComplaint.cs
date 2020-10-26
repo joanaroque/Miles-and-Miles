eccdc7a3f61ed93f1a6f9adc3311e399770ed61d
joanaroque <joanatpsi@gmail.com>
2020-10-26 15:27:29 +0000

    bugs fixed; added migrations in ClientProject

--- CinelAirMilesLibrary.Web/Data/Entities/ClientComplaint.cs
+++ CinelAirMilesLibrary.Web/Data/Entities/ClientComplaint.cs
@@ -8,7 +8,7 @@ namespace CinelAirMilesLibrary.Common.Data.Entities
     public class ClientComplaint : IEntity
     {
 
-        public ComplaintType Complaint { get; set; }
+       public ComplaintType Complaint { get; set; }
 
 
 
