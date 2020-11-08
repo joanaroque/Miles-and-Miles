5165b57f4ae92082dd95af0aeefba7f74d7f0674
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-11-08 18:55:21 +0000

    fixed JS to async load pages

--- CinelAirMiles/wwwroot/js/site.js
+++ CinelAirMiles/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/**
+ * Calls the action:Edit on the target item.
+ * The action belongs to UserController.
+ * Target item is stored in html identifier:
+ * 
+ * data-value
+ * */
+function openPartial() {
+    const id = event.currentTarget.getAttribute("data-value");
+    const action = event.currentTarget.getAttribute("action");
+
+    $('#form_container').load(getPath(document.location.pathname) + '/' + action + '?id=' + id);
+}
+
+
+
+
+/**
+ * Calls an action with the given parameters
+ * The action called is retrieved from the html identifiers:
+ * 
+ * action
+ * */
+function openPartialCreate() {
+    const action = event.currentTarget.getAttribute("action");
+
+    $('#form_container').load(getPath(document.location.pathname) + '/' + action);
+}
+
