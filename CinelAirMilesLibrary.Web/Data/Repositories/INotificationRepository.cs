1033e0b5b87a6bad4a14f6d124d0a2d99ced44b1
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-11-08 17:14:35 +0000

    Added new methods to Update and Delete

--- CinelAirMilesLibrary.Web/Data/Repositories/INotificationRepository.cs
+++ CinelAirMilesLibrary.Web/Data/Repositories/INotificationRepository.cs
@@ -13,10 +13,24 @@
         /// </summary>
         /// <param name="currentClient">current client</param>
         /// <returns>list notifications</returns>
-        List<Notification> GetAllNotifications(string currentClient);
+        IEnumerable<Notification> GetAllNotifications(string currentClient);
+
+
+        /// <summary>
+        /// Get the count of a list of notifications filtered by user role
+        /// and by Type PremiumOffer
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        Task<int> GetPremiumLenghtByRole(UserType role);
+
+
+        /// <summary>
+        /// Get a list of notifications by user role
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
         IEnumerable<Notification> GetNotificationsByRole(UserType role);
-
-
 
 
         /// <summary>
@@ -26,5 +40,27 @@
         /// <returns>list of unread notifications</returns>
         Task<Notification> GetUnreadNotifications(int clientId);
 
+        /// <summary>
+        /// Get the count of a list of notifications filtered by user role
+        /// and by Type Partner
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        Task<int> GetPartnerLenghtByRole(UserType role);
+
+        /// <summary>
+        /// Get the count of a list of notifications filtered by user role
+        /// and by Type Advertising
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        Task<int> GetAdvertLenghtByRole(UserType role);
+
+        /// <summary>
+        /// Get a notification by the Guid Id of the item it's attached to
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Notification> GetByGuidIdAsync(string id);
     }
 }
