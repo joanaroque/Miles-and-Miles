9264fec6531e5190f8c189f61e545b5c89da6430
Catia Oliveira <ctoliveira44@gmail.com>
2020-11-09 15:33:16 +0000

    added migration

---
+++ CinelAirMilesLibrary.Web/Migrations/20201109153151_ChangesInUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MilesBackOffice.Web.Migrations
+{
+    public partial class ChangesInUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Tier",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tier",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
