49f8e6848eed41f62890909bbc8cdcf34317dc57
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-11-09 19:25:12 +0000

    Added CreatedBy and CreatedOn

--- MilesTests/Models/AdvertisingViewModel.cs
+++ MilesTests/Models/AdvertisingViewModel.cs
@@ -14,7 +14,6 @@
         public IEnumerable<SelectListItem> PartnersList { get; set; }
 
 
-
         [Required]
         [Range(1, double.MaxValue, ErrorMessage = "Choose a Partner from the list.")]
         public int PartnerId { get; set; }
@@ -31,13 +30,10 @@
         public string Title { get; set; }
 
 
-
         public string Content { get; set; }
 
 
-
         public int Status { get; set; }
-
 
 
         [Display(Name = "Image Location")]
@@ -48,26 +44,18 @@
         public IFormFile ImageFile { get; set; }
 
 
-
         [Required(ErrorMessage = "Must insert the {0}")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
 
 
-        public string Observations { get; set; }
-
         public string PostGuidId { get; set; }
 
 
-
-        public string Arrival { get; set; }
-
+        public string CreatedBy { get; set; }
 
 
-        public string Departure { get; set; }
-
-
-        public DateTime FlightDateTime { get; set; }
+        public DateTime CreatedOn { get; set; }
     }
 }
