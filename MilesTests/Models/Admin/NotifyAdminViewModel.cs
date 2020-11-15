fce5f3dd5c1668d649c0f72a8a6a264d216ff133
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-11-15 15:59:52 +0000

    ViewModel

---
+++ MilesTests/Models/Admin/NotifyAdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CinelAirMilesLibrary.Common.Enums;
+
+namespace MilesBackOffice.Web.Models.Admin
+{
+    public class NotifyAdminViewModel : NewClientsViewModel
+    {
+        public string Notification { get; set; }
+
+        public string Message { get; set; }
+
+        public UserType SelectedRole { get; set; }
+
+    }
+}
